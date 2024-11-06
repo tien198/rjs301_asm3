52b8fbe1b57001cb9e38b0582c4f791e5f3f50d8
tien <tienvn998@gmail.com>
2024-11-06 21:07:21 +0700

    add TailwindCSS and react-router-dom

--- backend/data/util.js
+++ backend/data/util.js
@@ -1,21 +1,21 @@
 const fs = require('node:fs/promises');
 
 async function readData() {
-  const data = await fs.readFile('products.json', 'utf8');
+  const data = await fs.readFile('databaseDocs/products.json', 'utf8');
   return JSON.parse(data);
 }
 
 async function writeData(data) {
-  await fs.writeFile('products.json', JSON.stringify(data));
+  await fs.writeFile('databaseDocs/products.json', JSON.stringify(data));
 }
 
 async function readUserData() {
-  const data = await fs.readFile('users.json', 'utf8');
+  const data = await fs.readFile('databaseDocs/users.json', 'utf8');
   return JSON.parse(data);
 }
 
 async function writeUserData(data) {
-  await fs.writeFile('users.json', JSON.stringify(data));
+  await fs.writeFile('databaseDocs/users.json', JSON.stringify(data));
 }
 
 exports.readData = readData;
